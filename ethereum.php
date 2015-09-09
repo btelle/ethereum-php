2da2b663dfbb699ec3c69b59d1cdbb3f2e4d4b87
Brandon Telle <brandon.telle@gmail.com>
2015-09-09 00:18:33 +0000

    Added more functions

--- ethereum.php
+++ ethereum.php
@@ -30,4 +30,143 @@ class Ethereum extends JSON_RPC
 	{
 		return $this->ether_request(__FUNCTION__, array($input));
 	}
+	
+	function net_version()
+	{
+		return $this->ether_request(__FUNCTION__);
+	}
+	
+	function net_listening()
+	{
+		return $this->ether_request(__FUNCTION__);
+	}
+	
+	function net_peerCount()
+	{
+		return $this->ether_request(__FUNCTION__);
+	}
+	
+	function eth_protocolVersion()
+	{
+		return $this->ether_request(__FUNCTION__);
+	}
+	
+	function eth_coinbase()
+	{
+		return $this->ether_request(__FUNCTION__);
+	}
+	
+	function eth_mining()
+	{
+		return $this->ether_request(__FUNCTION__);
+	}
+	
+	function eth_hashrate()
+	{
+		return $this->ether_request(__FUNCTION__);
+	}
+	
+	function eth_gasPrice()
+	{
+		return $this->ether_request(__FUNCTION__);
+	}
+	
+	function eth_accounts()
+	{
+		return $this->ether_request(__FUNCTION__);
+	}
+	
+	function eth_blockNumber()
+	{
+		return $this->ether_request(__FUNCTION__);
+	}
+	
+	function eth_getBalance($address, $block='latest')
+	{
+		return $this->ether_request(__FUNCTION__, array($address, $block));
+	}
+	
+	function eth_getStorageAt($address, $at, $block='latest')
+	{
+		return $this->ether_request(__FUNCTION__, array($address, $at, $block));
+	}
+	
+	function eth_getTransactionCount($address, $block='latest')
+	{
+		return $this->ether_request(__FUNCTION__, array($address, $block));
+	}
+	
+	function eth_getBlockTransactionCountByHash($tx_hash)
+	{
+		return $this->ether_request(__FUNCTION__, array($tx_hash));
+	}
+	
+	function eth_getBlockTransactionCountByNumber($tx='latest')
+	{
+		return $this->ether_request(__FUNCTION__, array($tx));
+	}
+	
+	function eth_eth_getUncleCountByBlockHash($block_hash)
+	{
+		return $this->ether_request(__FUNCTION__, array($block_hash));
+	}
+	
+	function eth_getUncleCountByBlockNumber($block='latest')
+	{
+		return $this->ether_request(__FUNCTION__, array($block));
+	}
+	
+	function eth_getCode($address, $block='latest')
+	{
+		return $this->ether_request(__FUNCTION__, array($address, $block));
+	}
+	
+	function eth_sign($address, $input)
+	{
+		return $this->ether_request(__FUNCTION__, array($address, $input));
+	}
+	
+	function eth_sendTransaction($transaction)
+	{
+		if(!is_a($transaction, 'Ethereum_Transaction')
+		{
+			throw new ErrorException('Transaction object expected');
+		}
+		else
+		{
+			return $this->ether_request(__FUNCTION__, $transaction->to_param_array());	
+		}
+	}
+}
+
+class Ethereum_Transaction
+{
+	private $to, $from, $gas, $gasPrice, $value, $data, $nonce;
+	
+	function __construct($from, $to, $gas, $gasPrice, $value, $data='', $nonce=NULL)
+	{
+		$this->from = $from;
+		$this->to = $to;
+		$this->gas = $gas;
+		$this->gasPrice = $gasPrice;
+		$this->value = $value;
+		$this->data = $data;
+		$this->nonce = $nonce;
+	}
+	
+	function to_param_array()
+	{
+		return array(
+			array
+			(
+				'from'=>$this->from,
+				'to'=>$this->to,
+				'gas'=>$this->gas,
+				'gasPrice'=>$this->gasPrice,
+				'value'=>$this->value,
+				'data'=>$this->data,
+				'nonce'=>$this->nonce
+			)
+		);
+	}
 }
--- test/test.php
+++ test/test.php
@@ -9,3 +9,17 @@ $eth = new Ethereum('127.0.0.1', 8545);
 
 echo $eth->web3_clientVersion().'<br>';
 echo $eth->web3_sha3('0x68656c6c6f20776f726c64').'<br>';
+echo $eth->net_version().'<br>';
+echo ($eth->net_listening()===TRUE?'True':'False').'<br>';
+echo $eth->net_peerCount().'<br>';
+echo $eth->eth_protocolVersion().'<br>';
+var_dump($eth->eth_coinbase()); echo '<br>';
+echo ($eth->eth_mining()===TRUE?'True':'False').'<br>';
+echo $eth->eth_hashrate().'<br>';
+echo $eth->eth_gasPrice().'<br>';
+print_r($eth->eth_accounts()); echo '<br>';
+echo $eth->eth_blockNumber().'<br>';
+echo $eth->eth_getBalance('0x407d73d8a49eeb85d32cf465507dd71d507100c1').'<br>';
+echo $eth->eth_getStorageAt('0x407d73d8a49eeb85d32cf465507dd71d507100c1', '0x0', '0x2').'<br>';
+echo $eth->eth_getTransactionCount('0x407d73d8a49eeb85d32cf465507dd71d507100c1').'<br>';
+var_dump($eth->eth_getBlockTransactionCountByHash('0xb903239f8543d04b5dc1ba6579132b143087c68db1b2168786408fcbce568238')); echo '<br>';
